faeddab17deffc87b5177a77b7cca374c7220a8f
Neil Locketz <nlocketz@gmail.com>
2014-12-18 06:27:12 +0000

    gtk window is sorta working... we might be able to put the webview in shared memory

---
+++ src/dds_gtk.h
@@ -0,0 +1,8 @@
+#ifndef DDS_GTK
+#define DDS_GTK
+#include <gtk/gtk.h>
+#include <webkit/webkit.h>
+
+WebKitWebView *make_view(char* init_url);
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,23 +1,29 @@
 
+#include <unistd.h>
+#include "dds_gtk.h"
 #include "dict.h"
 #include "readcfg.h"
 #include "ddsmsgqueue.h"
-
+#include "dds_gtk.h"
 #define KEY_PATH "/home/pi/c-dds/src/main.c"
+
 int main(void){
 	int qid = get_q_id(KEY_PATH, 'd');
 	Dict* d = readConfig("../Configs/PIE.conf");
 
-	
-
-
-
+	WebKitWebView* view = make_view((char*)dict_get_val(d, "init_page"));	
+	if(!fork()){
+		gtk_main();
+	}
 
 
 
 	destroy_q(qid);
 	//Deallocates the dictionary
 	delete_dict_and_contents(d);
+	printf("Main Process is waiting...\n");
+	webkit_web_view_load_uri(view, "http://facebook.com");
+	wait(NULL);
 }
 
 
