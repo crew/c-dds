7ff800a3a2bc57a12fe61d697d0df2928fc72e0f
Neil Locketz <nlocketz@gmail.com>
2014-12-19 21:02:41 +0000

    got page switching from another thread working...\n

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <pthread.h>
 
 #include <unistd.h>
 #include "dds_gtk.h"
@@ -6,24 +7,29 @@
 #include "ddsmsgqueue.h"
 #include "dds_gtk.h"
 #define KEY_PATH "/home/pi/c-dds/src/main.c"
+void* gtk_thread(void* arg){
+	WebKitWebView* view = (WebKitWebView*)arg;
+	sleep(10);
+	printf("Switching pages...\n");
+	gdk_threads_enter();
+	webkit_web_view_load_uri(view, "http://facebook.com");
+	gdk_threads_leave();
 
+}
 int main(void){
-	int qid = get_q_id(KEY_PATH, 'd');
+	pthread_t gtk_main_thread;
 	Dict* d = readConfig("../Configs/PIE.conf");
 
 	WebKitWebView* view = make_view((char*)dict_get_val(d, "init_page"));	
-	if(!fork()){
-		gtk_main();
-	}
-
-
-
-	destroy_q(qid);
+	pthread_create(&gtk_main_thread, NULL, gtk_thread, (void*) view);
+	gtk_main();
+	//webkit_web_view_load_uri(view, "http://facebook.com");
+	//printf("Right before the sleep...\n");
+	//sleep(10);
+	//printf("Right after the sleep...\n");
 	//Deallocates the dictionary
 	delete_dict_and_contents(d);
-	printf("Main Process is waiting...\n");
-	webkit_web_view_load_uri(view, "http://facebook.com");
-	wait(NULL);
+
 }
 
 
