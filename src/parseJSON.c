a766b5905b495ee95dd811e6e0faafefe961ca36
belph <peblairman@gmail.com>
2014-12-28 13:02:00 -0500

    Updated JSON Parsing to fit the Python Server's...NOTE: The Python's sending out content as a string instead of a JSON object breaks the parser. Also, I've added a .gitignore to avoid accidentally committing any debugging binaries.

--- src/parseJSON.c
+++ src/parseJSON.c
@@ -9,7 +9,7 @@
 #include "parseJSON.h"
 
 
-slide_action parse_action(char *str) {
+SLIDE_ACTION parse_action(char *str) {
     if (~strcmp(str, "add-slide")) {
         return ADD_SLIDE;
     }
@@ -29,11 +29,11 @@ slide_action parse_action(char *str) {
         syslog(LOG_MAKEPRI(LOG_SYSLOG, LOG_ERR),
                 "parse_action: Recieved an invalid slide action");
         closelog();
-        return (slide_action) NULL;
+        return (SLIDE_ACTION) NULL;
     }
 }
 
-char *action_string(slide_action action) {
+char *action_string(SLIDE_ACTION action) {
     if (action == ADD_SLIDE) {
         return "add-slide";
     }
@@ -58,18 +58,85 @@ char *action_string(slide_action action) {
 }
 
 pie *parse_pie(cJSON *raw_pie) {
-    char *name = cJSON_GetObjectItem(raw_pie, "name")->valuestring;
+    //char *name = cJSON_GetObjectItem(raw_pie, "name")->valuestring;
+	char *name = raw_pie->valuestring;
     pie *to_ret = malloc(sizeof(pie) + sizeof(name));
     to_ret->name = name;
     return to_ret;
 }
 
-cJSON *pie_to_json(pie *parsed_pie) {
-    cJSON *to_ret = cJSON_CreateObject();
-    cJSON_AddStringToObject(to_ret, "name", parsed_pie->name);
-    return to_ret;
+// In case we need to do anything fancy down the road
+char *pie_to_json(pie *parsed_pie) {
+    //cJSON *to_ret = cJSON_CreateObject();
+    //cJSON_AddStringToObject(to_ret, "name", parsed_pie->name);
+    return parsed_pie->name;
 }
 
+slide_action_info *make_slide_action_info(int id, char *location, int duration){
+	slide_action_info *to_ret = malloc(sizeof(slide_action_info));
+	to_ret->id = id;
+	to_ret->location = location;
+	to_ret->duration = duration;
+	return to_ret;
+}
+
+action_data *parse_action_data(cJSON *raw_action_data){
+	action_data *to_ret = malloc(sizeof(action_data));
+	char *action_type = cJSON_GetObjectItem(raw_action_data, "type")->valuestring;
+	if(!strcmp(action_type,"slide")){
+		to_ret->type = ADT_SLIDE;
+		to_ret->slide_data = make_slide_action_info(
+				cJSON_GetObjectItem(raw_action_data, "ID")->valueint,
+				cJSON_GetObjectItem(raw_action_data, "location")->valuestring,
+				cJSON_GetObjectItem(raw_action_data, "duration")->valueint
+		);
+		return to_ret;
+	}
+	printf("parse_action_data: WARNING: Given unimplemented slide action. Returning Null Pointer.\n");
+	return NULL;
+}
+
+cJSON *action_data_to_json(action_data *to_parse){
+	cJSON *to_ret = cJSON_CreateObject();
+	switch(to_parse->type){
+	case ADT_SLIDE:
+		cJSON_AddStringToObject(to_ret, "type", "slide");
+		cJSON_AddNumberToObject(to_ret, "ID", to_parse->slide_data->id);
+		cJSON_AddStringToObject(to_ret, "location", to_parse->slide_data->location);
+		cJSON_AddNumberToObject(to_ret, "duration", to_parse->slide_data->duration);
+		break;
+	default:
+		break;
+	}
+	return to_ret;
+}
+
+void parse_actions(cJSON *raw_actions, socket_message_content *reciever){
+	int len = cJSON_GetArraySize(raw_actions);
+	reciever->num_actions = len;
+	reciever->actions = malloc(len * sizeof(action_data *));
+	// Check if something went wrong
+	if (reciever->actions == NULL) {
+	    syslog(LOG_MAKEPRI(LOG_SYSLOG, LOG_ERR), "parse_actions: Could not allocate actions array for received message's content\n");
+	    return;
+	}
+	int i;
+	for(i = 0; i < len; i++){
+		reciever->actions[i] = parse_action_data(cJSON_GetArrayItem(raw_actions, i));
+	}
+	return;
+}
+
+cJSON *actions_to_json(action_data *actions[], int num_actions){
+	cJSON *to_ret = cJSON_CreateArray();
+	int i;
+	for(i = 0; i < num_actions; i ++){
+		cJSON_AddItemToArray(to_ret, action_data_to_json(actions[i]));
+	}
+	return to_ret;
+}
+
+/*
 void parse_pies(cJSON *pie_array, socket_message *to_set) {
     int len = cJSON_GetArraySize(pie_array);
     to_set->pie_list_len = len;
@@ -91,7 +158,7 @@ cJSON *pie_arr_to_json(pie *pie_array[], int len) {
         cJSON_AddItemToArray(ret_arr, pie_to_json(pie_array[i]));
     }
     return ret_arr;
-}
+}*/
 
 const char *parse_date(const char *input, struct tm *tm) {
     const char *cp;
@@ -246,15 +313,22 @@ socket_message *json_to_message(char *str) {
     input = cJSON_Parse(str);
     content = cJSON_GetObjectItem(input, "content");
     socket_message_content *msg_c = (socket_message_content *) malloc(sizeof(socket_message_content));
-    msg_c->id = cJSON_GetObjectItem(content, "ID")->valueint;
-    msg_c->permalink = cJSON_GetObjectItem(content, "Permalink")->valuestring;
-    msg_c->meta = parse_json_meta(cJSON_GetObjectItem(content, "meta"));
+    //msg_c->id = cJSON_GetObjectItem(content, "ID")->valueint;
+    //msg_c->permalink = cJSON_GetObjectItem(content, "Permalink")->valuestring;
+    parse_actions(cJSON_GetObjectItem(content,"actions"),msg_c);
+    cJSON *mt;
+    if(mt = cJSON_GetObjectItem(content, "meta")){ // Make sure meta is in JSON
+    	msg_c->meta = parse_json_meta(mt);
+    }
     socket_message *msg = (socket_message *) malloc(sizeof(socket_message));
     msg->datetime = malloc(sizeof(struct tm));
     parse_date(cJSON_GetObjectItem(input, "datetime")->valuestring, msg->datetime);
     msg->action = parse_action(cJSON_GetObjectItem(input, "action")->valuestring);
     msg->content = msg_c;
-    parse_pies(cJSON_GetObjectItem(input, "pies"), msg);
+    //parse_pies(cJSON_GetObjectItem(input, "pies"), msg);
+    msg->src = parse_pie(cJSON_GetObjectItem(input, "src"));
+    msg->dest = parse_pie(cJSON_GetObjectItem(input, "dest"));
+    msg->plugin_dest = cJSON_GetObjectItem(input, "pluginDest")->valuestring;
     return msg;
 }
 
@@ -266,11 +340,19 @@ char *message_to_json(socket_message *msg) {
     strftime(datetime, sizeof(datetime), "%FT%T%z", msg->datetime);
     cJSON_AddStringToObject(root, "datetime", datetime);
     cJSON_AddStringToObject(root, "action", action_string(msg->action));
-    cJSON_AddItemToObject(root, "pies", pie_arr_to_json(msg->pie_list, msg->pie_list_len));
+    //cJSON_AddItemToObject(root, "pies", pie_arr_to_json(msg->pie_list, msg->pie_list_len));
+    cJSON_AddStringToObject(root, "src", pie_to_json(msg->src));
+    cJSON_AddStringToObject(root, "dest", pie_to_json(msg->dest));
+    if(msg->plugin_dest){
+    	cJSON_AddStringToObject(root, "pluginDest", msg->plugin_dest);
+    }
     cJSON_AddItemToObject(root, "content", content = cJSON_CreateObject());
-    cJSON_AddNumberToObject(content, "ID", msg->content->id);
-    cJSON_AddStringToObject(content, "Permalink", msg->content->permalink);
-    cJSON_AddItemToObject(content, "meta", parse_message_meta(msg->content->meta));
+    //cJSON_AddNumberToObject(content, "ID", msg->content->id);
+    //cJSON_AddStringToObject(content, "Permalink", msg->content->permalink);
+    cJSON_AddItemToObject(content, "actions", actions_to_json(msg->content->actions, msg->content->num_actions));
+    if(msg->content->meta){
+    	cJSON_AddItemToObject(content, "meta", parse_message_meta(msg->content->meta));
+    }
     return cJSON_Print(root);
 }
 
@@ -352,30 +434,24 @@ Dict *meta_to_dict(Dict *meta){
 		}
 	}
 	to_ret->next = meta_to_dict(meta->next);
-
+	return to_ret;
 }
-/*
+
 //Testing function...compile with gcc parseJSON.c cJSON.c dict.c -lm -lrt
-char json_string[] = "{\"datetime\" : \"2014-11-30T22:04:15+0000\",\"action\" : \"add-slide\",\"pies\" : [{ \"name\" : \"shepard\" },{ \"name\" : \"blueberry\" }],\"content\"  : {\"ID\" : 14,\"Permalink\" : \"http://dds-wp...\", \"meta\" : {\"key1\" : [ \"value\" ],\"key2\" : [\"value1\",\"value2\",3,{\"meta can be weird\" : \"remember that\"}]}}}";
+//char json_string[] = "{\"datetime\" : \"2014-11-30T22:04:15+0000\",\"action\" : \"add-slide\",\"pies\" : [{ \"name\" : \"shepard\" },{ \"name\" : \"blueberry\" }],\"content\"  : {\"ID\" : 14,\"Permalink\" : \"http://dds-wp...\", \"meta\" : {\"key1\" : [ \"value\" ],\"key2\" : [\"value1\",\"value2\",3,{\"meta can be weird\" : \"remember that\"}]}}}";
+//char json_string[] = "{\"src\" : \"WPHandler\", \"dest\" : \"keylime\", \"datetime\" : \"2014-11-30T22:04:15+0000\", \"content\" : {\"actions\" : [{\"ID\" : 226,\"type\" : \"slide\",\"location\" : \"http:\\/\\/www.ccs.neu.edu\\/systems\\/labstats\\/212.html\",\"duration\" : 1},{\"ID\" : 194,\"type\" : \"slide\",\"location\" : \"http:\\/\\/10.0.0.202\\/weather2\\/\",\"duration\" : 15}]}, \"pluginDest\" : \"slideShow\", \"action\" : \"load-slides\"}";
+char json_string[] = "{\"src\" : \"WPHandler\", \"dest\" : \"keylime\", \"datetime\" : \"2014-11-30T22:04:15+0000\", \"content\" : {\"actions\" : [{\"ID\" : 226,\"type\" : \"slide\",\"location\" : \"http:\\/\\/www.ccs.neu.edu\\/systems\\/labstats\\/212.html\",\"duration\" : 1},{\"ID\" : 194,\"type\" : \"slide\",\"location\" : \"http:\\/\\/10.0.0.202\\/weather2\\/\",\"duration\" : 15}], \"meta\" : {\"key1\" : [ \"value\" ],\"key2\" : [\"value1\",\"value2\",3,{\"meta can be weird\" : \"remember that\"}]}}, \"pluginDest\" : \"slideShow\", \"action\" : \"load-slides\"}";
 
 int main() {
     socket_message *sm = json_to_message(json_string);
     #define DT_BUF_SIZE 27
     char dt[ DT_BUF_SIZE ];
     strftime(dt, sizeof(dt), "%Y-%m-%dT%H:%M:%S%z", sm->datetime);
-    printf("sm:\ndatetime:%s\naction:%s\npies:\n\t(name: %s)\n\t(name: %s)\ncontent:\n\tid: %d\n...\n",
-            dt, action_string(sm->action), sm->pie_list[0]->name, sm->pie_list[1]->name, sm->content->id);
-    //printf(dump_dict(sm->content->meta));
-    dump_meta(sm->content->meta);
-    //printf("sm->content->meta->key2->2: %d\n", *(int*)dict_get_val(meta_to_dict(sm->content->meta),"key2","2"));
-    //printf(dump_dict(((Dict *)(sm->content->meta->value))->next->next->next->value));
+    printf("sm:\ndatetime: %s\naction: %s\nsrc: %s\ndest: %s\ncontent:\n\t\n...\n",
+            dt, action_string(sm->action), sm->src->name, sm->dest->name);
+    if(sm->content->meta){
+    	dump_meta(sm->content->meta);
+    	printf("sm->content->meta->key2->2: %d\n", *(int*)dict_get_val(meta_to_dict(sm->content->meta),"key2","2"));
+    }
     printf("back again:\n%s\n", message_to_json(sm));
-    //printf("sm:\naction:%s\npies:\n\t(name: %s)\n\t(name: %s)\ncontent:\n\tid: %d\n...\n", action_string(sm->action), sm->pie_list[0]->name, sm->pie_list[1]->name, sm->content->id);
 }
-*/
-
-
-
-
-
-
--- src/parseJSON.h
+++ src/parseJSON.h
@@ -9,13 +9,30 @@
 #include "dict.h"
 #endif
 
+#include <stdarg.h>
+
+#define SERVER_PI (pie){"DDS_SERVER"}
+#define REG_ACTION_INFO(type,strct) typedef struct strct type##_action_info
+
 typedef enum {
     ADD_SLIDE, DELETE_SLIDE, EDIT_SLIDE, LOAD_SLIDES, TERMINATE
-} slide_action;
+} SLIDE_ACTION;
 
 typedef enum {
     T_INT, T_CHAR, T_POINT_INT, T_POINT_CHAR, T_POINT_VOID, T_ARR, T_DICT
 } META_TYPE;
+
+typedef enum {ADT_SLIDE} ACTION_DATA_TYPE;
+
+REG_ACTION_INFO(slide,{int id; char *location; int duration;});
+
+typedef struct action_data_struct {
+	ACTION_DATA_TYPE type;
+	union{ // In case we would like additional actions later,
+		   //  this union encapsulates everything slide-specific
+		 slide_action_info *slide_data;
+	};
+} action_data;
 
 typedef struct pie_struct {
     char *name;
@@ -27,17 +44,18 @@ typedef struct m_meta_struct{
 } socket_meta;
 
 typedef struct m_content_struct {
-    int id;
-    char *permalink;
     Dict *meta;
+    int num_actions;
+    action_data **actions;
 } socket_message_content;
 
 typedef struct m_root_struct {
     struct tm *datetime;
-    slide_action action;
+    SLIDE_ACTION action;
+    char *plugin_dest;
     socket_message_content *content;
-    int pie_list_len;
-    pie **pie_list;
+    pie *src;
+    pie *dest;
 } socket_message;
 
 extern socket_message *json_to_message(char *str);
