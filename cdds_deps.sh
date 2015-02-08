51e86cfc7bc4e2502fd518d804ffb0dfb09a7414
Neil Locketz <nlocketz@gmail.com>
2015-02-08 14:51:55 -0500

    Python - although i doubt we will need this later

--- cdds_deps.sh
+++ cdds_deps.sh
@@ -2,6 +2,6 @@ echo Adding gcc 4.9 Repository
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo apt-get update
 echo Installing deps
-sudo apt-get install -y gtk+-3.0 libwebkitgtk-3.0* lua5.1*
+sudo apt-get install -y gtk+-3.0 libwebkitgtk-3.0* lua5.1* python-dev
 sudo apt-get update
 
